34320c9eebc017f5ca4f75dab16b9161eacda545
Jet <taijunjet@hotmail.com>
2024-06-28 16:16:30 +0900

    add scripts

---
+++ scripts/train_mlp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source venv/bin/activate
+wingman-compress-weights
+
+declare -a pids=()
+python3 src/train_mlp_env.py &
+pids+=($!)
+sleep 10
+python3 src/train_mlp_env.py &
+pids+=($!)
+sleep 10
+python3 src/train_mlp_env.py &
+pids+=($!)
+sleep 10
+python3 src/train_mlp_env.py &
+pids+=($!)
+sleep 10
+
+for pid in ${pids[*]}; do
+    wait $pid
+done
