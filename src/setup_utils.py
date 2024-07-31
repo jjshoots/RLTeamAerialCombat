d660853aac5d6d63b37f6e5b5d2c4edae46a70c6
Jet <junjet.tai@sony.com>
2024-07-31 15:03:43 +0900

    bump gymnasium version

--- src/setup_utils.py
+++ src/setup_utils.py
@@ -5,8 +5,6 @@ import torch
 from gymnasium.vector import AsyncVectorEnv, VectorEnv
 from gymnasium.wrappers import rescale_action
 from pettingzoo import ParallelEnv
-from PyFlyt.gym_envs import FlattenWaypointEnv
-from PyFlyt.pz_envs import MAFixedwingDogfightEnv
 from wingman import Wingman
 from wingman.replay_buffer import ReplayBuffer
 
@@ -18,23 +16,44 @@ from dogfighter.models.mlp.mlp_qu_network import MlpQUNetworkConfig
 
 
 def setup_single_environment(wm: Wingman) -> gym.Env:
-    # define one env
-    env = gym.make(
-        wm.cfg.env_name,
-        render_mode="human" if wm.cfg.mode.display or wm.cfg.mode.render else None,
-        flight_mode=-1,
-    )
-    env = rescale_action.RescaleAction(env, min_action=-1.0, max_action=1.0)
+    if wm.cfg.env_name.startswith("PyFlyt"):
+        from PyFlyt.gym_envs import FlattenWaypointEnv
 
-    # wrap in flatten if needed
-    if "waypoint" in wm.cfg.env_name.lower():
-        env = FlattenWaypointEnv(env, context_length=1)
+        # define one env
+        env = gym.make(
+            wm.cfg.env_name,
+            render_mode="human" if wm.cfg.mode.display or wm.cfg.mode.render else None,
+            flight_mode=-1,
+        )
+        env = rescale_action.RescaleAction(env, min_action=-1.0, max_action=1.0)
 
-    # record observation space shape
-    if not getattr(wm.cfg, "obs_size", None):
-        wm.cfg.obs_size = env.observation_space.shape[0]  # pyright: ignore[reportOptionalSubscript]
-    if not getattr(wm.cfg, "act_size", None):
-        wm.cfg.act_size = env.action_space.shape[0]  # pyright: ignore[reportOptionalSubscript]
+        # wrap in flatten if needed
+        if "waypoint" in wm.cfg.env_name.lower():
+            env = FlattenWaypointEnv(env, context_length=1)
+
+        # record observation space shape
+        if not getattr(wm.cfg, "obs_size", None):
+            wm.cfg.obs_size = env.observation_space.shape[0]  # pyright: ignore[reportOptionalSubscript]
+        if not getattr(wm.cfg, "act_size", None):
+            wm.cfg.act_size = env.action_space.shape[0]  # pyright: ignore[reportOptionalSubscript]
+
+    elif wm.cfg.env_name.startswith("dm_control"):
+        import shimmy
+        gym.register_envs(shimmy)
+
+        env = gym.make(
+            wm.cfg.env_name,
+            render_mode="human" if wm.cfg.mode.display or wm.cfg.mode.render else None,
+        )
+
+        # record observation space shape
+        if not getattr(wm.cfg, "obs_size", None):
+            wm.cfg.obs_size = env.observation_space.shape[0]  # pyright: ignore[reportOptionalSubscript]
+        if not getattr(wm.cfg, "act_size", None):
+            wm.cfg.act_size = env.action_space.shape[0]  # pyright: ignore[reportOptionalSubscript]
+
+    else:
+        raise NotImplementedError
 
     return env
 
@@ -48,6 +67,8 @@ def setup_vector_environment(wm: Wingman) -> VectorEnv:
 
 
 def setup_ma_environment(wm: Wingman) -> ParallelEnv:
+    from PyFlyt.pz_envs import MAFixedwingDogfightEnv
+
     # define one env
     ma_env = MAFixedwingDogfightEnv(
         team_size=wm.cfg.team_size,
