cc45df01b18aa61b933111fe95b21ac6301075c9
Jet <taijunjet@hotmail.com>
2024-08-04 12:29:48 +0900

    narrow aggressiveness range

---
+++ scripts/sync_and_display.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -d .venv ]; then
+  source .venv/bin/activate
+else
+  source venv/bin/activate
+fi
+wingman-compress-weights
+
+rsync -rav --progress --stats "icy-pony:~/Sandboxes/RLTeamAerialCombat/weights/$1" ./weights
+python3 src/main.py --mode.display --model.id="$1"
