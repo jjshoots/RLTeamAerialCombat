4f19d90ea94e417d1999b66795035002666c253d
Jet <taijunjet@hotmail.com>
2024-08-05 13:57:34 +0900

    update sweep

--- scripts/run_sweep.sh
+++ scripts/run_sweep.sh
@@ -8,33 +8,18 @@ fi
 wingman-compress-weights
 
 declare -a pids=()
-wandb agent jjshoots/dogfighter/kbxzk5v8 &
+wandb agent jjshoots/dogfighter/lbyv6x8y &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/dogfighter/kbxzk5v8 &
+wandb agent jjshoots/dogfighter/lbyv6x8y &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/dogfighter/kbxzk5v8 &
+wandb agent jjshoots/dogfighter/lbyv6x8y &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/dogfighter/kbxzk5v8 &
+wandb agent jjshoots/dogfighter/lbyv6x8y &
 pids+=($!)
 sleep 10
-if [ "$hostname" != "arctic-linx" ]; then
-  # don't abuse my main PC
-  wandb agent jjshoots/dogfighter/kbxzk5v8 &
-  pids+=($!)
-  sleep 10
-  wandb agent jjshoots/dogfighter/kbxzk5v8 &
-  pids+=($!)
-  sleep 10
-  wandb agent jjshoots/dogfighter/kbxzk5v8 &
-  pids+=($!)
-  sleep 10
-  wandb agent jjshoots/dogfighter/kbxzk5v8 &
-  pids+=($!)
-  sleep 10
-fi
 
 for pid in ${pids[*]}; do
     wait $pid
