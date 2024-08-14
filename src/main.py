010992b455cc9b7a7c679b7edef61cb5ed684ad2
Jet <taijunjet@hotmail.com>
2024-08-15 06:39:00 +0900

    stash

--- src/main.py
+++ src/main.py
@@ -101,8 +101,3 @@ if __name__ == "__main__":
         display(wm)
     else:
         print("So this is life now.")
-
-    if wm.cfg.post_run.shutdown:
-        import os
-        os.system("shutdown -s")
-
