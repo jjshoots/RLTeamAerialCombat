230f56e4302c378b84dc04be3ed5cd69dbdf3275
Jet <taijunjet@hotmail.com>
2024-09-20 01:32:42 +0900

    update sweep

--- src/run_sweep.sh
+++ src/run_sweep.sh
@@ -4,13 +4,13 @@ source venv/bin/activate
 
 declare -a pids=()
 
-wandb agent jjshoots/obs_norm/bf00af52 &
+wandb agent jjshoots/obs_norm/ytg6s7md &
 pids+=($!)
-wandb agent jjshoots/obs_norm/bf00af52 &
+wandb agent jjshoots/obs_norm/ytg6s7md &
 pids+=($!)
-wandb agent jjshoots/obs_norm/bf00af52 &
+wandb agent jjshoots/obs_norm/ytg6s7md &
 pids+=($!)
-wandb agent jjshoots/obs_norm/bf00af52 &
+wandb agent jjshoots/obs_norm/ytg6s7md &
 pids+=($!)
 
 for pid in ${pids[*]}; do
